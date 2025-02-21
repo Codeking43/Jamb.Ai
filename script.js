6bf2326455a3ce7bef908b7bea62442482d4d25a
Codeking43 <mathiasmonfung65@gmail.com>
2025-02-21 05:25:11 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+fetch('https://questions.aloc.com.ng/api/v2/q?subject=crk', {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      AccessToken: 'QB-16de424c38e2779aa1f1',
+    },
+    method: 'GET',
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('data', data);
+      document.getElementById('subject').innerHTML = data.subject;
+      document.getElementById('question').innerHTML = data.data.question;
+      document.getElementById('optionA').innerHTML=data.data.option.a;
+      document.getElementById('optionB').innerHTML=data.data.option.b;
+      document.getElementById('optionC').innerHTML=data.data.option.c;
+      document.getElementById('optionD').innerHTML=data.data.option.d;
+    })
+    .catch(error => {
+      console.log('error', error);
+    });
